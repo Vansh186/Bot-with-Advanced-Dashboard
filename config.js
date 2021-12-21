35f1b184bd3ef6273cc9fd2553e6e991a15ed212
Vansh186 <78458980+Vansh186@users.noreply.github.com>
2021-12-21 22:34:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ module.exports = {
  "description": "",
  "domain": "", // This Domain need to be without https:// or http:// (eg: hydrabot.fun not https://hydrabot.fun)
  "google_analitics": "", 
- "token": "",
+ "token": "OTIyODk3MTY0NjU5NzQwNzQ1.YcII9g.Vm0ZQS7_guiC9ZWgKfwX00Qd1LM",
  "https":"https://",
  "port":"5003",
- "client_id":"",
- "secret":"",
+ "client_id":"922897164659740745",
+ "secret":"CWlVnHqFpbsl5CCFflYOz0kFXFrQeuBF",
  "support_server_link":"",
- "bot_name":""
+ "bot_name":"Radon"
 }
